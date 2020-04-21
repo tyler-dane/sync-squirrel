c362c34e8088edb56deb9e3c56940b29392fcfe7
Ty Hitzeman <tyler.hitzeman@gmail.com>
2020-04-20 20:39:41 -0500

    update config.yaml

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from app.constants import Con
 
 
 class Config:
-    CONFIG_PATH = "/Users/ty/src/github/sync-squirrel/config.dev.yaml"
+    CONFIG_PATH = "config.yaml"
     CONFIG_DATA = util.get_config_data_yaml(CONFIG_PATH)
 
     ######
